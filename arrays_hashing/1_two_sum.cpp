6c071709e130af543281568b97e32804035f595f
gabrielgcma <amadocmgabriel@gmail.com>
2024-02-12 23:32:34 -0300

    1. two sum

---
+++ arrays_hashing/1_two_sum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 0; j < nums.size(); j++) {
+        if (i != j && (nums[i] + nums[j] == target)) {
+          return vector<int>{i, j};
+        }
+      }
+    }
+    return vector<int>{};
+  }
+};
